15980b16579259849c22327d407194fbdcf5969f
Iurii Smyrnov <y_smirnov@ukr.net>
2014-10-30 16:56:59 +0200

    added eShop-service-interface project

---
+++ eShop/eShop-service-interface/src/main/java/com/ua/myprojects/eshop/service/CategoriesServiceInterface.java
@@ -0,0 +1,16 @@
+package com.ua.myprojects.eshop.service;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.ua.myprojects.eshop.service.model.Category;
+
+@Path("/categories")
+public interface CategoriesServiceInterface {
+	@GET
+	@Produces("application/json")
+	List<Category> queryCategories();
+}
---
+++ eShop/eShop-service-interface/src/main/java/com/ua/myprojects/eshop/service/model/Category.java
@@ -0,0 +1,14 @@
+package com.ua.myprojects.eshop.service.model;
+
+public class Category {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
