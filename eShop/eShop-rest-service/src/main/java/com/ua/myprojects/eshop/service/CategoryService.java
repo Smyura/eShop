5353286b4704a997def5879d11af2531cb954a87
Iurii Smyrnov <y_smirnov@ukr.net>
2015-02-27 17:25:02 +0200

    temporary commit WITH BUG - properties don't work

--- eShop/eShop-rest-service/src/main/java/com/ua/myprojects/eshop/service/CategoryService.java
+++ eShop/eShop-rest-service/src/main/java/com/ua/myprojects/eshop/service/CategoryService.java
@@ -1,5 +1,6 @@
 package com.ua.myprojects.eshop.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -23,39 +24,40 @@ public class CategoryService implements CategoryServiceInterface {
 		// TODO lets use AOP here
 		logger.info("--- Starting " + Thread.currentThread().getStackTrace()[1].getMethodName() + " ...");
 
-		logger.info("--- -> " + jdbc.getProperty("HOST"));
-		// Category category1 = new Category();
-		// category1.setTitle("Notebooks, Pads, PCs");
-		// List<String> names1 = new ArrayList<String>();
-		// names1.add("Notebooks");
-		// names1.add("Pads");
-		// names1.add("PCs");
-		// category1.setNames(names1);
-		//
-		// Category category2 = new Category();
-		// category2.setTitle("Phones");
-		// List<String> names2 = new ArrayList<String>();
-		// names2.add("Mobile phones");
-		// names2.add("Radio phones");
-		// category2.setNames(names2);
-		//
-		// Category category3 = new Category();
-		// category3.setTitle("Home technics");
-		// List<String> names3 = new ArrayList<String>();
-		// names3.add("Air conditioners");
-		// names3.add("Frizers");
-		// names3.add("TVs");
-		// category3.setNames(names3);
-		//
-		// categories = new ArrayList<Category>();
-		// categories.add(category1);
-		// categories.add(category2);
-		// categories.add(category3);
+		logger.info("--- -> " + jdbc.getProperty("host"));
+
+		Category category1 = new Category();
+		category1.setTitle("Notebooks, Pads, PCs");
+		List<String> names1 = new ArrayList<String>();
+		names1.add("Notebooks");
+		names1.add("Pads");
+		names1.add("PCs");
+		category1.setNames(names1);
+
+		Category category2 = new Category();
+		category2.setTitle("Phones");
+		List<String> names2 = new ArrayList<String>();
+		names2.add("Mobile phones");
+		names2.add("Radio phones");
+		category2.setNames(names2);
+
+		Category category3 = new Category();
+		category3.setTitle("Home technics");
+		List<String> names3 = new ArrayList<String>();
+		names3.add("Air conditioners");
+		names3.add("Frizers");
+		names3.add("TVs");
+		category3.setNames(names3);
+
+		List<Category> categories = new ArrayList<Category>();
+		categories.add(category1);
+		categories.add(category2);
+		categories.add(category3);
 
 		// TODO lets use AOP here
 		logger.info("--- Ending " + Thread.currentThread().getStackTrace()[1].getMethodName() + " !");
 
-		return null;
+		return categories;
 	}
 
 	// @Override
